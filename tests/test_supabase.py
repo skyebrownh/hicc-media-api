a7518c8c96865ab3883955b15063d06feb7817ec
Skye Brown <skye.brownh@gmail.com>
2025-06-18 18:28:24 -0400

    test validate_response() function

--- tests/test_supabase.py
+++ tests/test_supabase.py
@@ -1,8 +1,13 @@
+import pytest
 from app.utils.supabase import *
 
 def test_table_id():
-    table_id1 = table_id("users")
-    table_id2 = table_id("user_availability")
+    assert table_id("users") == "user_id"
+    assert table_id("user_availability") == "user_availability_id"
 
-    assert table_id1 == "user_id"
-    assert table_id2 == "user_availability_id"
+def test_validate_response():
+    with pytest.raises(HTTPException):
+        validate_response(None)
+    
+    with pytest.raises(HTTPException):
+        validate_response([])
