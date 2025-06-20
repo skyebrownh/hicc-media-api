d8c50ed2b212e6bafc00aa2f79c4bc205d169841
Skye Brown <skye.brownh@gmail.com>
2025-06-20 17:56:24 -0400

    pytest supabase update and delete

--- tests/test_supabase.py
+++ tests/test_supabase.py
@@ -18,15 +18,37 @@ def test_supabase_get_all(supabase_service, clean_teams_table):
 
 def test_supabase_get(supabase_service, clean_teams_table):
     # setup: insert teams
-    teamA = supabase_service.post("teams", body={"team_name": "Team A", "lookup": "teamA"})
-    teamB = supabase_service.post("teams", body={"team_name": "Team B", "lookup": "teamB"})
+    team_a = supabase_service.post("teams", body={"team_name": "Team A", "lookup": "team_a"})
+    team_b = supabase_service.post("teams", body={"team_name": "Team B", "lookup": "team_b"})
 
-    responseA = supabase_service.get("teams", id=teamA.get("team_id"))
-    responseB = supabase_service.get("teams", id=teamB.get("team_id"))
+    responseA = supabase_service.get("teams", id=team_a.get("team_id"))
+    responseB = supabase_service.get("teams", id=team_b.get("team_id"))
 
     assert responseA[0].get("team_name") == "Team A"
-    assert responseB[0].get("lookup") == "teamB"
-    assert responseB[0].get("team_id") == teamB.get("team_id")
+    assert responseB[0].get("lookup") == "team_b"
+    assert responseB[0].get("team_id") == team_b.get("team_id")
+
+def test_supabase_update(supabase_service, clean_teams_table):
+    # setup: insert team
+    update_team = supabase_service.post("teams", body={"team_name": "UPDATE ME", "lookup": "updateteam"})
+
+    supabase_service.update("teams", body={"team_name": "NEW"}, id=update_team.get("team_id"))
+    response = supabase_service.get("teams", id=update_team.get("team_id"))
+
+    assert response[0].get("team_name") == "NEW"
+    assert response[0].get("lookup") == "updateteam"
+
+def test_supabase_delete(supabase_service, clean_teams_table):
+    # setup: insert team
+    delete_team = supabase_service.post("teams", body={"team_name": "DELETE ME", "lookup": "deleteme"})
+
+    delete_response = supabase_service.delete("teams", id=delete_team.get("team_id"))
+
+    assert delete_response.get("team_id") == delete_team.get("team_id")
+    assert delete_response.get("lookup") == "deleteme" 
+
+    with pytest.raises(HTTPException):
+        supabase_service.get("teams", id=delete_team.get("team_id"))
 
 # helper functions
 def test_table_id():
