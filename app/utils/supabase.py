c7bfe9541e918bcaf45642eea0d4cc41883e7a68
Skye Brown <skye.brownh@gmail.com>
2025-06-20 15:55:11 -0400

    refactor supabase utility as class, add test env vars, pytest supabase post

--- app/utils/supabase.py
+++ app/utils/supabase.py
@@ -1,36 +1,34 @@
 from fastapi import HTTPException
 from supabase import create_client, Client
 
-from .env import SUPABASE_URL, SUPABASE_API_KEY 
+class SupabaseService():
+    def __init__(self, url, key):
+        self.client: Client = create_client(url, key)
 
-url = SUPABASE_URL
-key = SUPABASE_API_KEY
-supabase: Client = create_client(url, key)
+    def get(self, table: str, id: str | None = None):
+        query = self.client.table(table).select("*")
 
-def supabase_get(table: str, id: str | None = None):
-    query = supabase.table(table).select("*")
+        if id:
+            query = query.eq(table_id(table), id)
 
-    if id:
-        query = query.eq(table_id(table), id)
+        response = query.execute()
+        validate_response(response)
+        return response.data
 
-    response = query.execute()
-    validate_response(response)
-    return response.data
+    def post(self, table: str, body: dict):
+        response = self.client.table(table).insert(body).execute()
+        validate_response(response)
+        return response.data[0]
 
-def supabase_post(table: str, body: dict):
-    response = supabase.table(table).insert(body).execute()
-    validate_response(response)
-    return response.data[0]
+    def update(self, table: str, body: dict, id: str):
+        response = self.client.table(table).update(body).eq(table_id(table), id).execute()
+        validate_response(response)
+        return response.data[0]
 
-def supabase_update(table: str, body: dict, id: str):
-    response = supabase.table(table).update(body).eq(table_id(table), id).execute()
-    validate_response(response)
-    return response.data[0]
-
-def supabase_delete(table: str, id: str):
-    response = supabase.table(table).delete().eq(table_id(table), id).execute()
-    validate_response(response)
-    return response.data[0]
+    def delete(self, table: str, id: str):
+        response = self.client.table(table).delete().eq(table_id(table), id).execute()
+        validate_response(response)
+        return response.data[0]
 
 # helper functions
 def table_id(table: str) -> str:
--- tests/test_supabase.py
+++ tests/test_supabase.py
@@ -1,6 +1,14 @@
 import pytest
 from app.utils.supabase import *
+from app.utils.env import SUPABASE_TEST_URL, SUPABASE_TEST_API_KEY
 
+def test_supabase_post():
+    supabase = SupabaseService(url=SUPABASE_TEST_URL, key=SUPABASE_TEST_API_KEY)
+    response = supabase.post("teams", body={"team_name": "TEST TEAM", "lookup": "testteam"})
+    assert response.get("team_name") == "TEST TEAM"
+    assert response.get("lookup") == "testteam"
+
+# helper functions
 def test_table_id():
     assert table_id("users") == "user_id"
     assert table_id("user_availability") == "user_availability_id"
