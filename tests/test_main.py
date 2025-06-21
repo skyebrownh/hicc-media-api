135b25b9103c7d37c12e300f60367956820a2c78
Skye Brown <skye.brownh@gmail.com>
2025-06-20 23:44:37 -0400

    test get single user route

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,3 @@
-import pytest
-
 def test_get_all_users(test_client, setup_user):
     response = test_client.get("/users")
     assert response.status_code == 200
@@ -7,3 +5,12 @@ def test_get_all_users(test_client, setup_user):
     response_json = response.json()
     assert len(response_json) > 0
     assert response_json[0].get("user_id") == setup_user.get("user_id")
+
+def test_get_single_user(test_client, setup_user):
+    response = test_client.get(f"/users/{setup_user.get("user_id")}")
+    assert response.status_code == 200
+
+    response_json = response.json()
+    assert len(response_json) == 1
+    assert response_json[0].get("user_id") == setup_user.get("user_id")
+    assert response_json[0].get("first_name") == "TEST"
