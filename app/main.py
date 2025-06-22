a4d07ade9cb78d991a5c610c150473246f2eaa83
Skye Brown <skye.brownh@gmail.com>
2025-06-22 13:23:26 -0400

    add media_roles and proficiency_level routes that pass tests

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
-from app.routers import users, teams
+from app.routers import users, teams, media_roles, proficiency_levels
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(teams.router)
+app.include_router(media_roles.router)
+app.include_router(proficiency_levels.router)
