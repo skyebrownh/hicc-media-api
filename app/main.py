6e37b1d25dbe86daa02cacb2ae05ccc4079eb92c
Skye Brown <skye.brownh@gmail.com>
2025-06-22 14:13:37 -0400

    add schedule date types and dates routes that pass tests

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.routers import users, teams, media_roles, proficiency_levels
+from app.routers import users, teams, media_roles, proficiency_levels, schedule_date_types, dates
 
 app = FastAPI()
 
@@ -8,3 +8,5 @@ app.include_router(users.router)
 app.include_router(teams.router)
 app.include_router(media_roles.router)
 app.include_router(proficiency_levels.router)
+app.include_router(schedule_date_types.router)
+app.include_router(dates.router)
