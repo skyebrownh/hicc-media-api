7f63c5b82920f3089ba9b4124c8ab2e404d22c8b
Skye Brown <skye.brownh@gmail.com>
2025-06-23 10:44:39 -0400

    fix date in test user availability update, add user availability routes that pass tests

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -5,7 +5,7 @@ from .proficiency_levels import router
 from .schedule_date_types import router
 from .dates import router
 from .schedules import router
-# from .user_availability import router
+from .user_availability import router
 # from .team_users import router
 # from .user_roles import router
 # from .schedule_dates import router
