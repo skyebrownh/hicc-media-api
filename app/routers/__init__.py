7310877a504072e4416986159ce3215d90a2f99f
Skye Brown <skye.brownh@gmail.com>
2025-06-23 10:48:15 -0400

    add team users routes that pass tests

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -6,7 +6,7 @@ from .schedule_date_types import router
 from .dates import router
 from .schedules import router
 from .user_availability import router
-# from .team_users import router
+from .team_users import router
 # from .user_roles import router
 # from .schedule_dates import router
 # from .schedule_date_roles import router
