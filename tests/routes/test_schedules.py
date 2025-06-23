10a34b5e5e3c09e78a7e6a6ac448ff284dfd97ea
Skye Brown <skye.brownh@gmail.com>
2025-06-23 10:26:51 -0400

    fix FK errors in schedule tests, add schedules routes that pass tests

--- tests/routes/test_schedules.py
+++ tests/routes/test_schedules.py
@@ -15,10 +15,10 @@ def test_get_single_schedule(test_client, setup_schedule):
     assert response_json[0].get("schedule_id") == setup_schedule.get("schedule_id")
     assert response_json[0].get("month_start_date") == "2000-01-01" 
 
-def test_post_schedule(test_client, clean_schedules_table):
+def test_post_schedule(test_client, setup_schedule):
     invalid_json1 = {}
     invalid_json2 = {"month_start_date": "INVALID"}
-    valid_json = {"month_start_date": "2002-01-01"}
+    valid_json = {"month_start_date": "2000-01-01", "is_active": False, "notes": "post schedule test"}
 
     response1 = test_client.post("/schedules", json=invalid_json1)
     assert response1.status_code == 422 
@@ -29,19 +29,19 @@ def test_post_schedule(test_client, clean_schedules_table):
     response = test_client.post("/schedules", json=valid_json)
     response_json = response.json()
     assert response.status_code == 200 
-    assert response_json.get("month_start_date") == "2002-01-01"
-    assert response_json.get("is_active") == True 
+    assert response_json.get("month_start_date") == "2000-01-01"
+    assert response_json.get("is_active") == False 
 
 def test_update_schedule(test_client, setup_schedule):
     setup_schedule_id = setup_schedule.get("schedule_id")
 
     empty_json = {}
     invalid_json2 = {"schedule_id": "00000000-0000-0000-0000-000000000000"}
-    valid_json = {"month_start_date": "2007-01-01", "notes": "updated"}
+    valid_json = {"notes": "updated"}
 
     response1 = test_client.patch(f"/schedules/{setup_schedule_id}", json=empty_json)
     assert response1.status_code == 404 
-    assert setup_schedule.get("month_start_date") == "2000-01-01"
+    assert setup_schedule.get("notes") == None 
 
     response2 = test_client.patch(f"/schedules/{setup_schedule_id}", json=invalid_json2)
     assert response2.status_code == 404
@@ -52,7 +52,6 @@ def test_update_schedule(test_client, setup_schedule):
     response = test_client.patch(f"/schedules/{setup_schedule_id}", json=valid_json)
     response_json = response.json()
     assert response.status_code == 200
-    assert response_json.get("month_start_date") == "2007-01-01"
     assert response_json.get("notes") == "updated"
 
 def test_delete_schedule(test_client, setup_schedule):
